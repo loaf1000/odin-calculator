56cd8e9e0167b16e39c49579e6874be2d8ec8811
Laithe Marshall <laithe.marshall@hivemedia.com>
2024-06-04 14:34:37 -0700

    Maths

---
+++ script.js
@@ -0,0 +1,43 @@
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function multiply (num1, num2)
+{
+    return num1 * num2;
+}
+
+function divide (num1, num2)
+{
+    if (num2 == 0)
+        {
+            return "error";
+        }
+
+    return num1/num2;
+}
+
+function operate (num1, num2, operator)
+{
+    switch(operator) {
+        case "+":
+            console.log(add(num1, num2));
+            break;
+        case "-":
+            console.log(subtract(num1, num2));
+            break;
+        case "*":
+            console.log(multiply(num1, num2));
+            break;
+        case "/":
+            console.log(divide(num1, num2));
+            break;
+    }
+
+}
