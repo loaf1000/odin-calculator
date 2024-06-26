49d448bd1339b7bad0e693fb2c9a3b878610027d
Laithe Marshall <loaf1000@gmail.com>
2024-06-25 17:14:20 -0700

    hooked up buttons

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButtons = document.querySelectorAll(".clear-button");
+const display = document.querySelector("#display");
+
+let operator;
+let number1;
+let number2;
+let operatorUsed = false;
+
+
 function add(num1, num2)
 {
     return num1 + num2;
@@ -27,17 +38,57 @@ function operate (num1, num2, operator)
 {
     switch(operator) {
         case "+":
-            console.log(add(num1, num2));
+            return add(num1, num2);
             break;
         case "-":
-            console.log(subtract(num1, num2));
+            return subtract(num1, num2);
             break;
         case "*":
-            console.log(multiply(num1, num2));
+            return multiply(num1, num2);
             break;
-        case "/":
-            console.log(divide(num1, num2));
+        case "÷":
+            return divide(num1, num2);
             break;
     }
 
 }
+
+function setup()
+{
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            display.innerText += button.innerText;
+        });
+    });
+    operatorButtons.forEach((button) => {
+        if (button.innerText == "=" )
+            {
+                button.addEventListener("click", () => {
+                    if (number1 > 0 || !isNaN(number1))
+                        {
+                            let index = display.innerText.indexOf(operator);
+                            number2 = Number(display.innerText.slice(index + 1));
+                            display.innerText = operate(number1, number2, operator);
+                            operatorUsed = false;
+                        }
+                });
+            }
+        else
+        {
+            button.addEventListener("click", () => {
+                if (!operatorUsed && display.innerText !="")
+                    {
+                        number1 = Number(display.innerText);
+                        display.innerText += button.innerText;
+                        operator = button.innerText;
+                        operatorUsed = true;
+                    }
+            });
+        }
+
+
+    });
+    
+}
+
+setup();
